e362b9d938d99680757742967b48fc946c374a07
jamolkhon <Jamolkhon.Kosimkhonov@pickles.com.au>
2023-09-05 16:59:37 +0800

    updated about

--- src/utils/bin/about.ts
+++ src/utils/bin/about.ts
@@ -1,7 +1,13 @@
 import { getBio } from '../../api';
 
 export const about = async (args: string[]): Promise<string> => {
-  const bio = await getBio();
+  // const bio = await getBio();
 
-  return bio;
+  return `
+  Hi 👋, I'm Jamolkhon. I am 22 years old student, seeking opportunities to enlarge 
+  my knowledge and experience in cyber security field. My goal is to leverage my experience 
+  and knowledge to make a positive impact in the cybersecurity industry. I am eager to learn 
+  from experienced professionals in the field and to contribute my skills and expertise. 
+  I'm always eager to expand my network and learn from others in the field.
+  `
 };
