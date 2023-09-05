eed1ab09827d78accd801786a7f1662d6769e2fa
jamolkhon <Jamolkhon.Kosimkhonov@pickles.com.au>
2023-09-05 17:21:18 +0800

    updated readme

--- src/utils/bin/utils.ts
+++ src/utils/bin/utils.ts
@@ -5,7 +5,13 @@ import * as bin from './index';
 export const help = async (args: string[]): Promise<string> => {
   const commands = Object.keys(bin).sort().join(', ');
 
-  return `Available commands:\n${commands}\n\n[tab]\t trigger completion.\n[ctrl+l] clear terminal.\n[ctrl+c] cancel command.`;
+  return `Available commands:\n
+  ${commands}
+  \n\n
+  ----\n
+  [tab]\t trigger completion.\n
+  [ctrl+l] clear terminal.\n
+  [ctrl+c] cancel command.`;
 };
 
 export const echo = async (args: string[]): Promise<string> => {
